d6b3843561e085787070d4f767b8affefe0385df
yujin <kimyu28jin@gmail.com>
2021-10-04 16:46:23 +0900

    Feat : home 화면 렌더링 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Repo } from 'hooks/useRepository';
+import SearchBar from './SearchBar';
+import RepoList from './RepoList';
+import styled from 'styled-components';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [query, setQuery] = useState('');
+  const [searchList, setSearchList] = useState<Repo[]>([]);
+
+  return (
+    <Layout>
+      <SearchBar {...{ query, setQuery, searchList, setSearchList }} />
+      <RepoList {...{ searchList }} />
+    </Layout>
+  );
 };
 
 export default Home;
+
+const Layout = styled.section`
+  background: #181a26;
+  min-height: 100vh;
+`;
