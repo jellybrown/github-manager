139e7d787e0ef742671a34a856b01358aad43d0c
yujin <kimyu28jin@gmail.com>
2021-10-07 20:45:49 +0900

    Feat : 날짜 변환 추가

--- src/utils/extract.ts
+++ src/utils/extract.ts
@@ -1,6 +1,10 @@
 import { Issue, IssueState } from 'types';
 import { Repo } from 'hooks/useRepository';
 
+const dateToKr = (date: string): string => {
+  return new Date(date).toLocaleString('KO-KR');
+};
+
 const editText = (text: null | string) => {
   if (text === null) return '';
   else if (text.length <= 450) return text;
@@ -14,8 +18,8 @@ export const extractRepoContent = (data: any[]): Repo[] => {
       id: item.id,
       fullName: item.full_name,
       description: editText(item.description),
-      createdAt: item.created_at,
-      updatedAt: item.updated_at,
+      createdAt: dateToKr(item.created_at),
+      updatedAt: dateToKr(item.updated_at),
       language: item.language,
       stargazersCount: item.stargazers_count,
       authorAvatarUrl: item.owner.avatar_url,
