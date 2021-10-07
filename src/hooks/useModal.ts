ba87c7bb38a46bc28d64421f1be5988149c9f113
yujin <kimyu28jin@gmail.com>
2021-10-07 21:05:00 +0900

    Style : import 부분 수정

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useModal = (initialState = false) => {
   const [opened, setOpened] = useState(initialState);
